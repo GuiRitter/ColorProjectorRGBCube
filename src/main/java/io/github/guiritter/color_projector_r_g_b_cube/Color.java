dca6c8bb94f6629faa7500daabd37c0a26173ed2
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2024-12-13 16:28:53 -0300

    WIP

--- src/main/java/io/github/guiritter/color_projector_r_g_b_cube/Color.java
+++ src/main/java/io/github/guiritter/color_projector_r_g_b_cube/Color.java
@@ -1,6 +1,11 @@
 package io.github.guiritter.color_projector_r_g_b_cube;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
+
+import static io.github.guiritter.color_projector_r_g_b_cube.ColorProjectorRGBCube.STEP;
+import static io.github.guiritter.color_projector_r_g_b_cube.ColorProjectorRGBCube.MAX_COLOR_VALUE;
 
 public class Color implements Comparable<Color> {
 
@@ -24,22 +29,22 @@ public class Color implements Comparable<Color> {
 	@Override
 	public int compareTo(Color o) {
 		if (g < o.g) {
-			return -1;
+			return 1;
 		}
 		if (g > o.g) {
-			return 1;
+			return -1;
 		}
 		if (b < o.b) {
-			return -1;
+			return 1;
 		}
 		if (b > o.b) {
-			return 1;
-		}
-		if (r < o.r) {
 			return -1;
 		}
-		if (r > o.r) {
+		if (r < o.r) {
 			return 1;
+		}
+		if (r > o.r) {
+			return -1;
 		}
 		return 0;
 	}
@@ -54,5 +59,29 @@ public class Color implements Comparable<Color> {
 	@Override
 	public int hashCode() {
 		return Objects.hash(r, g, b);
+	}
+
+	public List<Color> getNextColors() {
+		var list = new LinkedList<Color>();
+
+		var nextG = g + STEP;
+
+		if (nextG <= MAX_COLOR_VALUE) {
+			list.add(new Color(r, nextG, b));
+		}
+
+		var nextB = b + STEP;
+		
+		if (nextB <= MAX_COLOR_VALUE) {
+			list.add(new Color(r, g, nextB));
+		}
+
+		var nextR = r + STEP;
+
+		if (nextR <= MAX_COLOR_VALUE) {
+			list.add(new Color(nextR, g, b));
+		}
+
+		return list;
 	}
 }
--- src/main/java/io/github/guiritter/color_projector_r_g_b_cube/ColorProjectorRGBCube.java
+++ src/main/java/io/github/guiritter/color_projector_r_g_b_cube/ColorProjectorRGBCube.java
@@ -1,17 +1,44 @@
 package io.github.guiritter.color_projector_r_g_b_cube;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class ColorProjectorRGBCube {
 
 	public static final int MAX_COLOR_VALUE = 255;
+	public static final int MIN_COLOR_VALUE = 0;
+
+	// public static final int STEP = 17;
+	public static final int STEP = 255;
 
 	public static void main(String args[]) {
-		Color firstStartingPoint = new Color(0, 0, 0);
-		Color secondStartingPoint = null;
+		// Color firstStartingPoint = new Color(0, 0, 0);
+		// Color secondStartingPoint = null;
 
-		System.out.println(new Color(0, 300, 0));
+		// System.out.println(new Color(0, 300, 0));
 
-		// while (true) {
+		var setList = new LinkedList<Set<Color>>();
 
-		// }
+		setList.add(new TreeSet<Color>());
+
+		setList.getLast().add(new Color(0, 0, 0));
+
+		while (true) {
+			var currentSet = setList.getLast();
+
+			var nextSet = new TreeSet<Color>();
+
+			currentSet.forEach((color) -> nextSet.addAll(color.getNextColors()));
+
+			setList.add(nextSet);
+
+			if (nextSet.size() == 1) {
+				break;
+			}
+		}
+
+		System.out.println(setList);
 	}
 }
